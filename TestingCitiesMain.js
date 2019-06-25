ebe660f55faa450aa63266b078f08e94ead6537e
Sam <s.j.bozman@gmail.com>
2019-06-25 06:35:46 -0700

    Some Tweaks

--- TestingCitiesMain.js
+++ TestingCitiesMain.js
@@ -2,7 +2,9 @@ var cities = [];
 var citiesIndex = [];
 var cityPath = [];
 var bestPath = [];
-var totalCities = 100;
+//Set totalCities to population size of the Test.csv file
+var totalCities = 130;
+var tweak = 1; //Used to adjust city points on screen 
 var reached = [];
 var unreached = [];
 var dropdown;
@@ -17,15 +19,31 @@ var mr; //mutation rate (normally .01)
 var statusP;
 
 //****************************************************************** */
+function preload() {
+  table = loadTable('ch130.csv', 'csv','header');
+}
+//****************************************************************** */
 function setup() {
   createCanvas(1000, 1000);
-  mr = .0000001; //Mutation rate (between .01 and .0000001)
+  var mr = document.getElementById("myNumber").value;
+  //mr = .01; //Mutation rate (between .01 and .0000001)
 
-  for (var i = 0; i < totalCities; i++) {
-    var v = createVector(random(width), random(height), i);
-    cities[i] = v; //Array of city vectors
-    citiesIndex[i] = i; // Index order of city vectors     
+  // for (var i = 0; i < totalCities; i++) {
+  //   var v = createVector(random(width), random(height), i);
+  //   cities[i] = v; //Array of city vectors
+  //   citiesIndex[i] = i; // Index order of city vectors     
+  // }
+  
+  for (let r = 0; r < table.getRowCount(); r++){
+   
+    citiesIndex[r] = r;
+     
+    
+      var v = createVector(table.get(r, 'x')*tweak, table.get(r, 'y')*tweak,r);
+      cities[r] = v; //Initial random placement array of total 
+    
   }
+  
   
   //Set inital i to 1 so that I can push bestPath ot index 0
   for (var i = 1; i < shuffledArrays; i++) {
@@ -47,10 +65,10 @@ function draw() {
   nextGeneration();
   
   // Draw 4 quadrants on screen -------------------------------
-  stroke(155);
-  line(0, (height / 2), width, (height / 2));
-  stroke(155);
-  line(width / 2, 0, width / 2, height);
+  // stroke(155);
+  // line(0, (height / 2), width, (height / 2));
+  // stroke(155);
+  // line(width / 2, 0, width / 2, height);
 
   // Draw city connecting lines -------------------------------
   stroke(35, 191, 3);
@@ -68,16 +86,16 @@ function draw() {
 
   // Draw city circles and ID numbers -------------------------
   textSize(25);
-  textAlign(CENTER, BOTTOM);
+  textAlign(CENTER, TOP);
   for (var i = 0; i < cities.length; i++) {
     noFill();
     stroke(255);
     strokeWeight(4);
-    ellipse(cities[i].x, cities[i].y, 16, 16);
+    ellipse(cities[i].x, cities[i].y, 4, 4);
     // strokeWeight(2);
     // stroke(0, 0, 255);
     // textSize(25);
-    // text(i, cities[i].x, cities[i].y);
+    // text(i+1, cities[i].x, cities[i].y);
   }
 
 }
